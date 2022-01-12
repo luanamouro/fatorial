85abab864fb9aacf8277c5446a72de08f3028433
Luana <luana.mouro@gmail.com>
2022-01-12 18:08:57 -0300

    Fatorial

---
+++ fatorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, fat = 1, i; 
+            Console.WriteLine("Cálculo Fatorial\n\n");
+            Console.WriteLine("Digite um número para ver seu fatorial:");
+            numero = Convert.ToInt32(Console.ReadLine());
+            i = numero;
+
+            while(i>=1)
+
+            {
+                fat = fat * 1;
+                i--;
+            }
+            Console.WriteLine("Fatorial é {0}", fat.ToString());
+        }
+    }
+}
